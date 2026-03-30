b53624bad472e50e88bd56da0711836fac9b09f0
ANDREA DA CRUZ SILVA <andrea.csilva42@senacsp.edu.br>
2026-03-30 16:34:47 -0300

    Projeto

---
+++ Tela_Inicial/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Tela_Inicial
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gainsboro;
+            panel1.Location = new Point(-3, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1484, 56);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(-3, 55);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1484, 51);
+            panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(334, 7);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 39);
+            button1.TabIndex = 2;
+            button1.Text = "Novo Produto";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(642, 7);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 39);
+            button2.TabIndex = 2;
+            button2.Text = "Prontos";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(488, 7);
+            button3.Name = "button3";
+            button3.Size = new Size(127, 39);
+            button3.TabIndex = 2;
+            button3.Text = "Em Preparo";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(796, 7);
+            button4.Name = "button4";
+            button4.Size = new Size(127, 39);
+            button4.TabIndex = 2;
+            button4.Text = "Finalizados";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1470, 720);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ Tela_Inicial/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tela_Inicial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
