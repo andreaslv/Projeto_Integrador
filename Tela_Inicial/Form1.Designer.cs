751cc9847bab9f05e0fd8b71031656c6382c26ee
ANDREA DA CRUZ SILVA <andrea.csilva42@senacsp.edu.br>
2026-03-31 16:58:20 -0300

    Tela de controle

--- Tela_Inicial/Form1.Designer.cs
+++ Tela_Inicial/Form1.Designer.cs
@@ -29,81 +29,141 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            label1 = new Label();
             panel2 = new Panel();
-            button1 = new Button();
-            button2 = new Button();
             button3 = new Button();
             button4 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel1.SuspendLayout();
             panel2.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.Gainsboro;
+            panel1.Controls.Add(label1);
             panel1.Location = new Point(-3, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(1484, 56);
             panel1.TabIndex = 0;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 17.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(236, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(219, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Controle de Pedido";
+            // 
             // panel2
             // 
+            panel2.BackColor = SystemColors.ControlLight;
             panel2.Controls.Add(button3);
             panel2.Controls.Add(button4);
             panel2.Controls.Add(button2);
             panel2.Controls.Add(button1);
-            panel2.Location = new Point(-3, 55);
+            panel2.Location = new Point(0, 55);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1484, 51);
+            panel2.Size = new Size(1481, 51);
             panel2.TabIndex = 1;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(334, 7);
-            button1.Name = "button1";
-            button1.Size = new Size(127, 39);
-            button1.TabIndex = 2;
-            button1.Text = "Novo Produto";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(642, 7);
-            button2.Name = "button2";
-            button2.Size = new Size(127, 39);
-            button2.TabIndex = 2;
-            button2.Text = "Prontos";
-            button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(488, 7);
+            button3.BackColor = Color.FromArgb(217, 121, 36);
+            button3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(461, 7);
             button3.Name = "button3";
-            button3.Size = new Size(127, 39);
+            button3.Size = new Size(161, 39);
             button3.TabIndex = 2;
             button3.Text = "Em Preparo";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
-            button4.Location = new Point(796, 7);
+            button4.BackColor = Color.FromArgb(112, 115, 126);
+            button4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(795, 7);
             button4.Name = "button4";
-            button4.Size = new Size(127, 39);
+            button4.Size = new Size(161, 39);
             button4.TabIndex = 2;
             button4.Text = "Finalizados";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(88, 152, 57);
+            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(628, 7);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 39);
+            button2.TabIndex = 2;
+            button2.Text = "Prontos";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(13, 92, 182);
+            button1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(294, 7);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 39);
+            button1.TabIndex = 2;
+            button1.Text = "Novo Produto";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ControlLight;
+            panel3.Location = new Point(-3, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(214, 721);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ControlLightLight;
+            panel4.Controls.Add(panel5);
+            panel4.Location = new Point(217, 112);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1247, 603);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.Location = new Point(139, 47);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(417, 487);
+            panel5.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
             ClientSize = new Size(1470, 720);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             panel2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -115,5 +175,9 @@
         private Button button1;
         private Button button3;
         private Button button4;
+        private Panel panel3;
+        private Panel panel4;
+        private Label label1;
+        private Panel panel5;
     }
 }
