cea5cf4a0313786dea4f270750c34a06be4a2c29
ANDREA DA CRUZ SILVA <andrea.csilva42@senacsp.edu.br>
2026-04-02 14:29:48 -0300

    Tela de Lista de Pedidos

--- Tela_Inicial/Form1.Designer.cs
+++ Tela_Inicial/Form1.Designer.cs
@@ -38,9 +38,30 @@
             panel3 = new Panel();
             panel4 = new Panel();
             panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            label2 = new Label();
+            panel8 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
+            panel11 = new Panel();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             panel4.SuspendLayout();
+            panel6.SuspendLayout();
+            panel7.SuspendLayout();
+            panel8.SuspendLayout();
+            panel9.SuspendLayout();
+            panel10.SuspendLayout();
+            panel11.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -49,7 +70,7 @@
             panel1.Controls.Add(label1);
             panel1.Location = new Point(-3, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1484, 56);
+            panel1.Size = new Size(1475, 56);
             panel1.TabIndex = 0;
             // 
             // label1
@@ -71,7 +92,7 @@
             panel2.Controls.Add(button1);
             panel2.Location = new Point(0, 55);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1481, 51);
+            panel2.Size = new Size(1086, 51);
             panel2.TabIndex = 1;
             // 
             // button3
@@ -136,7 +157,7 @@
             panel4.Controls.Add(panel5);
             panel4.Location = new Point(217, 112);
             panel4.Name = "panel4";
-            panel4.Size = new Size(1247, 603);
+            panel4.Size = new Size(860, 603);
             panel4.TabIndex = 3;
             // 
             // panel5
@@ -146,12 +167,165 @@
             panel5.Size = new Size(417, 487);
             panel5.TabIndex = 0;
             // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.ControlDark;
+            panel6.Controls.Add(panel11);
+            panel6.Controls.Add(panel10);
+            panel6.Controls.Add(panel9);
+            panel6.Controls.Add(panel8);
+            panel6.Controls.Add(panel7);
+            panel6.Location = new Point(1083, 55);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(389, 666);
+            panel6.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.Silver;
+            panel7.Controls.Add(label2);
+            panel7.Location = new Point(0, 0);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(389, 51);
+            panel7.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(19, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(198, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Lista de Pedidos";
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.FromArgb(255, 255, 128);
+            panel8.Controls.Add(label7);
+            panel8.Controls.Add(label3);
+            panel8.Location = new Point(0, 72);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(389, 50);
+            panel8.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = Color.LightGreen;
+            panel9.Controls.Add(label8);
+            panel9.Controls.Add(label4);
+            panel9.Location = new Point(0, 128);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(389, 50);
+            panel9.TabIndex = 2;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = Color.FromArgb(224, 224, 224);
+            panel10.Controls.Add(label9);
+            panel10.Controls.Add(label5);
+            panel10.Location = new Point(0, 184);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(389, 50);
+            panel10.TabIndex = 2;
+            // 
+            // panel11
+            // 
+            panel11.BackColor = Color.FromArgb(224, 224, 224);
+            panel11.Controls.Add(label10);
+            panel11.Controls.Add(label6);
+            panel11.Location = new Point(0, 240);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(389, 50);
+            panel11.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(19, 15);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 21);
+            label3.TabIndex = 0;
+            label3.Text = "Pedido #01 -";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(19, 13);
+            label4.Name = "label4";
+            label4.Size = new Size(105, 21);
+            label4.TabIndex = 0;
+            label4.Text = "Pedido #01 -";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(19, 14);
+            label5.Name = "label5";
+            label5.Size = new Size(105, 21);
+            label5.TabIndex = 0;
+            label5.Text = "Pedido #01 -";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(19, 12);
+            label6.Name = "label6";
+            label6.Size = new Size(105, 21);
+            label6.TabIndex = 0;
+            label6.Text = "Pedido #01 -";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(126, 15);
+            label7.Name = "label7";
+            label7.Size = new Size(91, 21);
+            label7.TabIndex = 0;
+            label7.Text = "Em Preparo";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(126, 13);
+            label8.Name = "label8";
+            label8.Size = new Size(91, 21);
+            label8.TabIndex = 0;
+            label8.Text = "Em Preparo";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(126, 14);
+            label9.Name = "label9";
+            label9.Size = new Size(91, 21);
+            label9.TabIndex = 0;
+            label9.Text = "Em Preparo";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(126, 12);
+            label10.Name = "label10";
+            label10.Size = new Size(91, 21);
+            label10.TabIndex = 0;
+            label10.Text = "Em Preparo";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
             ClientSize = new Size(1470, 720);
+            Controls.Add(panel6);
             Controls.Add(panel4);
             Controls.Add(panel3);
             Controls.Add(panel2);
@@ -164,6 +338,17 @@
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
             panel4.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            panel9.ResumeLayout(false);
+            panel9.PerformLayout();
+            panel10.ResumeLayout(false);
+            panel10.PerformLayout();
+            panel11.ResumeLayout(false);
+            panel11.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -179,5 +364,20 @@
         private Panel panel4;
         private Label label1;
         private Panel panel5;
+        private Panel panel6;
+        private Panel panel11;
+        private Panel panel10;
+        private Panel panel9;
+        private Panel panel8;
+        private Panel panel7;
+        private Label label2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
     }
 }
