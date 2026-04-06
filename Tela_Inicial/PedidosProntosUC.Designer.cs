04d9668caf71d6f1bcb5457ef8007e12da93c651
AYLEE  MONTEIRO SOUZA SANTOS <aylee.mssantos@senacsp.edu.br>
2026-04-06 16:43:32 -0300

    oi

---
+++ Tela_Inicial/PedidosProntosUC.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Tela_Inicial
+{
+    partial class PedidosProntosUC
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            Pedido = new DataGridViewTextBoxColumn();
+            Observação = new DataGridViewTextBoxColumn();
+            Status = new DataGridViewTextBoxColumn();
+            horario = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(303, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Pedidos prontos";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Pedido, Observação, Status, horario });
+            dataGridView1.Location = new Point(52, 43);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(674, 300);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(52, 458);
+            button1.Name = "button1";
+            button1.Size = new Size(155, 23);
+            button1.TabIndex = 2;
+            button1.Text = "entregar pedido";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveBorder;
+            button2.Location = new Point(303, 458);
+            button2.Name = "button2";
+            button2.Size = new Size(168, 23);
+            button2.TabIndex = 3;
+            button2.Text = "voltar para o preparo";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveBorder;
+            button3.Location = new Point(536, 458);
+            button3.Name = "button3";
+            button3.Size = new Size(158, 23);
+            button3.TabIndex = 4;
+            button3.Text = "Cancelar pedido";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // Pedido
+            // 
+            Pedido.HeaderText = "Pedido";
+            Pedido.Name = "Pedido";
+            // 
+            // Observação
+            // 
+            Observação.HeaderText = "Observação";
+            Observação.Name = "Observação";
+            // 
+            // Status
+            // 
+            Status.HeaderText = "Status";
+            Status.Name = "Status";
+            // 
+            // horario
+            // 
+            horario.HeaderText = "horario";
+            horario.Name = "horario";
+            // 
+            // PedidosProntosUC
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "PedidosProntosUC";
+            Size = new Size(771, 551);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridViewTextBoxColumn Pedido;
+        private DataGridViewTextBoxColumn Observação;
+        private DataGridViewTextBoxColumn Status;
+        private DataGridViewTextBoxColumn horario;
+    }
+}
