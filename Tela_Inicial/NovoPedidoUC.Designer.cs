49dc4003a696b4e6b1d849e84654f931892bc699
ANDREA DA CRUZ SILVA <andrea.csilva42@senacsp.edu.br>
2026-04-08 14:18:46 -0300

    editado

--- Tela_Inicial/NovoPedidoUC.Designer.cs
+++ Tela_Inicial/NovoPedidoUC.Designer.cs
@@ -43,6 +43,13 @@
             label1 = new Label();
             label5 = new Label();
             panel1 = new Panel();
+            listBox1 = new ListBox();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -50,7 +57,7 @@
             // 
             button4.BackColor = Color.Silver;
             button4.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            button4.Location = new Point(313, 427);
+            button4.Location = new Point(131, 447);
             button4.Name = "button4";
             button4.Size = new Size(149, 38);
             button4.TabIndex = 26;
@@ -60,7 +67,7 @@
             // textBox1
             // 
             textBox1.BackColor = SystemColors.ScrollBar;
-            textBox1.Location = new Point(237, 107);
+            textBox1.Location = new Point(38, 87);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(315, 23);
             textBox1.TabIndex = 25;
@@ -70,9 +77,9 @@
             comboBox4.BackColor = SystemColors.ScrollBar;
             comboBox4.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             comboBox4.FormattingEnabled = true;
-            comboBox4.Location = new Point(455, 194);
+            comboBox4.Location = new Point(342, 168);
             comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(92, 25);
+            comboBox4.Size = new Size(109, 25);
             comboBox4.TabIndex = 24;
             comboBox4.Text = "Refrigerante";
             // 
@@ -82,9 +89,9 @@
             comboBox3.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             comboBox3.FormattingEnabled = true;
             comboBox3.Items.AddRange(new object[] { "Batata frita", "Onion Ring", "" });
-            comboBox3.Location = new Point(340, 194);
+            comboBox3.Location = new Point(175, 168);
             comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(94, 25);
+            comboBox3.Size = new Size(149, 25);
             comboBox3.TabIndex = 23;
             comboBox3.Text = "Acompanhamentos";
             // 
@@ -94,9 +101,9 @@
             comboBox2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "Smash onios", "Smash Melt duplo", "Bomba de chedar", "Smash Tasty ", "Smash Original Duplo ", "Smash Onions Duplo", "Googlebig", "Whatsauce ( X-Salada )", "Facebacon ( X-Bacon )", "Pcq ( X-burguer )", "Burger Tik Crock " });
-            comboBox2.Location = new Point(230, 194);
+            comboBox2.Location = new Point(57, 168);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(84, 25);
+            comboBox2.Size = new Size(94, 25);
             comboBox2.TabIndex = 22;
             comboBox2.Text = "Lanches";
             // 
@@ -105,18 +112,18 @@
             comboBox1.BackColor = SystemColors.ScrollBar;
             comboBox1.ForeColor = SystemColors.WindowFrame;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(237, 262);
+            comboBox1.Location = new Point(394, 87);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(292, 23);
+            comboBox1.Size = new Size(77, 23);
             comboBox1.TabIndex = 21;
             // 
             // button3
             // 
             button3.BackColor = Color.Silver;
             button3.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            button3.Location = new Point(455, 355);
+            button3.Location = new Point(281, 245);
             button3.Name = "button3";
-            button3.Size = new Size(75, 47);
+            button3.Size = new Size(75, 38);
             button3.TabIndex = 20;
             button3.Text = "Cartão";
             button3.UseVisualStyleBackColor = false;
@@ -125,9 +132,9 @@
             // 
             button2.BackColor = Color.Silver;
             button2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            button2.Location = new Point(344, 361);
+            button2.Location = new Point(170, 245);
             button2.Name = "button2";
-            button2.Size = new Size(75, 35);
+            button2.Size = new Size(75, 38);
             button2.TabIndex = 19;
             button2.Text = "Pix";
             button2.UseVisualStyleBackColor = false;
@@ -136,9 +143,9 @@
             // 
             button1.BackColor = Color.Silver;
             button1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            button1.Location = new Point(237, 361);
+            button1.Location = new Point(63, 245);
             button1.Name = "button1";
-            button1.Size = new Size(75, 35);
+            button1.Size = new Size(75, 38);
             button1.TabIndex = 18;
             button1.Text = "Dinheiro";
             button1.UseVisualStyleBackColor = false;
@@ -146,9 +153,10 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(219, 318);
+            label4.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            label4.Location = new Point(25, 206);
             label4.Name = "label4";
-            label4.Size = new Size(124, 15);
+            label4.Size = new Size(144, 19);
             label4.TabIndex = 17;
             label4.Text = "Forma de Pagamento:";
             // 
@@ -156,17 +164,17 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            label3.Location = new Point(220, 240);
+            label3.Location = new Point(377, 65);
             label3.Name = "label3";
-            label3.Size = new Size(66, 19);
+            label3.Size = new Size(42, 19);
             label3.TabIndex = 16;
-            label3.Text = "Endereço";
+            label3.Text = "Mesa";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            label2.Location = new Point(219, 154);
+            label2.Location = new Point(25, 129);
             label2.Name = "label2";
             label2.Size = new Size(109, 19);
             label2.TabIndex = 15;
@@ -176,7 +184,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            label1.Location = new Point(220, 85);
+            label1.Location = new Point(25, 65);
             label1.Name = "label1";
             label1.Size = new Size(113, 19);
             label1.TabIndex = 14;
@@ -200,10 +208,81 @@
             panel1.Size = new Size(771, 51);
             panel1.TabIndex = 27;
             // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(481, 151);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(268, 364);
+            listBox1.TabIndex = 28;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            label6.Location = new Point(588, 129);
+            label6.Name = "label6";
+            label6.Size = new Size(52, 19);
+            label6.TabIndex = 16;
+            label6.Text = "Pedido";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(25, 332);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(394, 23);
+            textBox2.TabIndex = 29;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            label7.Location = new Point(25, 301);
+            label7.Name = "label7";
+            label7.Size = new Size(92, 19);
+            label7.TabIndex = 17;
+            label7.Text = "Observações:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Symbol", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(131, 389);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 30);
+            label8.TabIndex = 30;
+            label8.Text = "Total:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Symbol", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(196, 389);
+            label9.Name = "label9";
+            label9.Size = new Size(37, 30);
+            label9.TabIndex = 30;
+            label9.Text = "R$";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI Symbol", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(230, 389);
+            label10.Name = "label10";
+            label10.Size = new Size(62, 30);
+            label10.TabIndex = 30;
+            label10.Text = "00,00";
+            // 
             // NovoPedidoUC
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(textBox2);
+            Controls.Add(listBox1);
             Controls.Add(panel1);
             Controls.Add(button4);
             Controls.Add(textBox1);
@@ -214,7 +293,9 @@
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(label7);
             Controls.Add(label4);
+            Controls.Add(label6);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -243,5 +324,12 @@
         private Label label1;
         private Label label5;
         private Panel panel1;
+        private ListBox listBox1;
+        private Label label6;
+        private TextBox textBox2;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
     }
 }
--- Tela_Inicial/PedidosProntosUC.Designer.cs
+++ Tela_Inicial/PedidosProntosUC.Designer.cs
@@ -28,68 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
             dataGridView1 = new DataGridView();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
             Pedido = new DataGridViewTextBoxColumn();
             Observação = new DataGridViewTextBoxColumn();
             Status = new DataGridViewTextBoxColumn();
             horario = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(303, 20);
-            label1.Name = "label1";
-            label1.Size = new Size(123, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Pedidos prontos";
             // 
             // dataGridView1
             // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Pedido, Observação, Status, horario });
-            dataGridView1.Location = new Point(52, 43);
+            dataGridView1.Location = new Point(52, 85);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(674, 300);
+            dataGridView1.Size = new Size(660, 300);
             dataGridView1.TabIndex = 1;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-            // 
-            // button1
-            // 
-            button1.BackColor = SystemColors.ActiveBorder;
-            button1.ForeColor = SystemColors.ActiveCaptionText;
-            button1.Location = new Point(52, 458);
-            button1.Name = "button1";
-            button1.Size = new Size(155, 23);
-            button1.TabIndex = 2;
-            button1.Text = "entregar pedido";
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            button2.BackColor = SystemColors.ActiveBorder;
-            button2.Location = new Point(303, 458);
-            button2.Name = "button2";
-            button2.Size = new Size(168, 23);
-            button2.TabIndex = 3;
-            button2.Text = "voltar para o preparo";
-            button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            button3.BackColor = SystemColors.ActiveBorder;
-            button3.Location = new Point(536, 458);
-            button3.Name = "button3";
-            button3.Size = new Size(158, 23);
-            button3.TabIndex = 4;
-            button3.Text = "Cancelar pedido";
-            button3.UseVisualStyleBackColor = false;
             // 
             // Pedido
             // 
@@ -111,25 +73,76 @@
             horario.HeaderText = "horario";
             horario.Name = "horario";
             // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(52, 458);
+            button1.Name = "button1";
+            button1.Size = new Size(155, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Entregar pedido";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveBorder;
+            button2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            button2.Location = new Point(291, 458);
+            button2.Name = "button2";
+            button2.Size = new Size(168, 34);
+            button2.TabIndex = 3;
+            button2.Text = "Voltar para o preparo";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveBorder;
+            button3.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            button3.Location = new Point(536, 458);
+            button3.Name = "button3";
+            button3.Size = new Size(158, 34);
+            button3.TabIndex = 4;
+            button3.Text = "Cancelar pedido";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(771, 51);
+            panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(291, 13);
+            label2.Name = "label2";
+            label2.Size = new Size(186, 25);
+            label2.TabIndex = 0;
+            label2.Text = "PEDIDOS PRONTOS";
+            // 
             // PedidosProntosUC
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
-            Controls.Add(label1);
             Name = "PedidosProntosUC";
             Size = new Size(771, 551);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
         private DataGridView dataGridView1;
         private Button button1;
         private Button button2;
@@ -138,5 +151,7 @@
         private DataGridViewTextBoxColumn Observação;
         private DataGridViewTextBoxColumn Status;
         private DataGridViewTextBoxColumn horario;
+        private Panel panel1;
+        private Label label2;
     }
 }
